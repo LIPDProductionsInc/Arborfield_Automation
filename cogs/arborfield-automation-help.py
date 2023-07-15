1e72e46fc044ac9cd4dd16c00132a610b5113728
LIPDProductionsInc <n00846739@gmail.com>
2023-07-15 00:43:49 -0400

    Better Help Command

--- cogs/arborfield-automation-help.py
+++ cogs/arborfield-automation-help.py
@@ -11,15 +11,32 @@ class HelpCog(commands.Cog, name="Help Cog"):
     @commands.hybrid_command(name="help")
     async def help(self, ctx, *, command: str = None):
         """Shows help about a command or the bot"""
-        embed = discord.Embed(
-            title="Help",
-            description="Here is a list of commands you can use with Arborfield Automation.",
-            color=discord.Color.dark_red()
-            )
-        embed.add_field(name="Commands", value="`help` `ping` `serverinfo` `userinfo` `avatar`", inline=False)
-        embed.add_field(name="Moderation", value="`ban` `kick` `unban`", inline=False)
-        embed.set_footer(text=f"Arborfield Automation | Developed by {self.bot.owner}", icon_url=str(self.bot.user.avatar))
-        await ctx.send(embed=embed)
+        if command is None:
+            embed = discord.Embed(
+                title="Help",
+                description="Here is a list of commands you can use with Arborfield Automation.",
+                color=discord.Color.dark_red()
+                )
+            if discord.utils.get(ctx.author.roles, id=581574409832366086):
+                embed.add_field(name="Council Commands", value="`propose`, `legal-review`, `charter`, `template`, `documents`", inline=False)
+            if discord.utils.get(ctx.author.roles, id=578723625390309390) or discord.utils.get(ctx.author.roles, id=806150833842421760) or discord.utils.get(ctx.author.roles, id=581574602212507648):
+                embed.add_field(name="Presiding Officer Commands", value="`docket`, `session`, `end-session`, `floor`, `dismiss`, `send`, `documents`, `elections`", inline=False)
+            if discord.utils.get(ctx.author.roles, id=580403860724776980):
+                embed.add_field(name="City Attorney Commands", value="`send`, `documents`", inline=False)
+            if discord.utils.get(ctx.author.roles, id=583496754712805376) or discord.utils.get(ctx.author.roles, id=1087922383698014279):
+                embed.add_field(name="City Clerk Commands", value="`transcript`, `bulletin`", inline=False)
+            guild_perms = None
+            if ctx.author.guild_permissions.ban_members:
+                guild_perms = "`ban` `unban` "
+            if ctx.author.guild_permissions.kick_members:
+                guild_perms += "`kick` "
+            if guild_perms is not None:
+                embed.add_field(name="Moderation", value=f"{guild_perms}", inline=False)
+            embed.add_field(name="Commands", value="`help` `ping` `serverinfo` `userinfo` `avatar`", inline=False)
+            embed.set_footer(text=f"Arborfield Automation | Developed by {self.bot.owner}", icon_url=str(self.bot.user.avatar))
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send("Help regarding a specific command is not yet implemented.")
         pass
 
         @commands.command(name="beta-help", description="Shows help about a command or the bot (Beta version)", aliases=["help-beta"], hidden=True)
