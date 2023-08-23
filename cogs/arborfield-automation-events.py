df596373a5b9eee598fbd9198a48e135808ab8b3
LIPDProductionsInc <n00846739@gmail.com>
2023-08-23 10:04:59 -0400

    Final Changes for the Reaction Trigger

--- cogs/arborfield-automation-events.py
+++ cogs/arborfield-automation-events.py
@@ -51,13 +51,13 @@ class EventsCog(commands.Cog, name="Events Cog"):
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
-#        if payload.channel_id == 854761365150629898:
-#            for role in payload.member.roles:
-#                if role.id != 581574409832366086:
-#                    channel = self.bot.get_channel(payload.channel_id)
-#                    message = await channel.fetch_message(payload.message_id)
-#                    await message.remove_reaction(payload.emoji, payload.member)
-        if payload.channel_id == 947186552839237674:
+        if payload.channel_id == 854761365150629898:
+            for role in payload.member.roles:
+                if role.id != 581574409832366086:
+                    channel = self.bot.get_channel(payload.channel_id)
+                    message = await channel.fetch_message(payload.message_id)
+                    await message.remove_reaction(payload.emoji, payload.member)
+        elif payload.channel_id == 947186552839237674:
             for role in payload.member.roles:
                 if role.id == 581574602212507648:
                     guild = self.bot.get_guild(payload.guild_id)
@@ -65,29 +65,25 @@ class EventsCog(commands.Cog, name="Events Cog"):
                     if message.author.id == 1033859464488562788:
                         direct_message = "<:Arborfield:1054103937239756800> | **LEAVE OF ABSENCE REQUEST**\n\n"
                         embed = message.embeds[0]
-                        field = embed.fields[0]
-                        id = field.value
-                        id = id[2:-1]
+                        footer = embed.footer.text
+                        id = footer[4:]
                         member = guild.get_member(int(id))
                         if payload.emoji.name == '✅':
                             direct_message += f"The Arborfield {role.name.title()} has approved your leave of absence request.\n\n"
                             direct_message += "You cannot be in-game or active in other departments while on leave. You are **NOT** allowed to be in attendance of any council sessions while on your leave; however, once you return (if the session is still ongoing), you may \"enter\" the session (make sure you reply to the attendance message).\n\n"
                             direct_message += "If you are currently in attendance for a discord session upon this approval message, the current Presiding Officer will excuse you from the rest of the sessions and note it for the record.\n\n"
-                            direct_message += f"Please contact {payload.membr.mention} if you have any questions.\n\n*{payload.member.display_name}*\n*Arborfield {role.name.title()}*"
+                            direct_message += f"Please contact {payload.member.mention} if you have any questions.\n\n*{payload.member.display_name}*\n*Arborfield {role.name.title()}*"
                             await member.send(direct_message)
                             print(f"{payload.member.display_name} approved {member.display_name}'s leave of absence request.")
-                        if payload.emoji.name == '❌':
+                        elif payload.emoji.name == '❌':
                             direct_message += f"The Arborfield {role.name.title()} has **denied** your leave of absence request.\n\n"
                             direct_message += f"If you have any questions, please contact {payload.member.mention}.\n\n"
                             direct_message += f"*{payload.member.display_name}*\n*Arborfield {role.name.title()}*"
                             await member.send(direct_message)
                             print(f"{payload.member.display_name} denied {member.display_name}'s leave of absence request.")
-#                else:
-#                    if payload.member.id != 1033859464488562788:
-#                        channel = self.bot.get_channel(payload.channel_id)
-#                        message = await channel.fetch_message(payload.message_id)
-#                        await message.remove_reaction(payload.emoji, payload.member)
-#                        pass
+                        else:
+                            raise ValueError("Invalid emoji.")
+                        pass
                     pass
                 pass
             pass
