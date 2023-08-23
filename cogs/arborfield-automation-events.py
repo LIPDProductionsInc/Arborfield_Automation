8f5389c0335a34114146452a67d6a0bc54648cb5
LIPDProductionsInc <n00846739@gmail.com>
2023-08-23 08:41:02 -0400

    Reaction Trigger

--- cogs/arborfield-automation-events.py
+++ cogs/arborfield-automation-events.py
@@ -57,6 +57,35 @@ class EventsCog(commands.Cog, name="Events Cog"):
                     channel = self.bot.get_channel(payload.channel_id)
                     message = await channel.fetch_message(payload.message_id)
                     await message.remove_reaction(payload.emoji, payload.member)
+        elif payload.channel_id == 947186552839237674:
+            for role in payload.member.roles:
+                if role.id == 581574602212507648:
+                    guild = self.bot.get_guild(payload.guild_id)
+                    message = await self.bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
+                    if message.author.id == 1033859464488562788:
+                        direct_message = "<:Arborfield:1054103937239756800> | **LEAVE OF ABSENCE REQUEST**\n\n"
+                        embed = message.embeds[0]
+                        field = embed.fields[0]
+                        id = field.value
+                        id = id[2:-1]
+                        member = guild.get_member(int(id))
+                        if payload.emoji.name == '✅':
+                            direct_message += f"The Arborfield {role.name.title()} has approved your leave of absence request.\n\n"
+                            direct_message += "You cannot be in-game or active in other departments while on leave. You are **NOT** allowed to be in attendance of any council sessions while on your leave; however, once you return (if the session is still ongoing), you may \"enter\" the session (make sure you reply to the attendance message).\n\n"
+                            direct_message += "If you are currently in attendance for a discord session upon this approval message, the current Presiding Officer will excuse you from the rest of the sessions and note it for the record.\n\n"
+                            direct_message += f"Please contact {payload.membr.mention} if you have any questions.\n\n*{payload.member.display_name}*\n*Arborfield {role.name.title()}*"
+                            await member.send(direct_message)
+                            print(f"{payload.member.display_name} approved {member.display_name}'s leave of absence request.")
+                        elif payload.emoji.name == '❌':
+                            direct_message += f"The Arborfield {role.name.title()} has **denied** your leave of absence request.\n\n"
+                            direct_message += f"If you have any questions, please contact {payload.member.mention}.\n\n"
+                            direct_message += f"*{payload.member.display_name}*\n*Arborfield {role.name.title()}*"
+                            await member.send(direct_message)
+                            print(f"{payload.member.display_name} denied {member.display_name}'s leave of absence request.")
+                else:
+                    channel = self.bot.get_channel(payload.channel_id)
+                    message = await channel.fetch_message(payload.message_id)
+                    await message.remove_reaction(payload.emoji, payload.member)
                     pass
                 pass
             pass
